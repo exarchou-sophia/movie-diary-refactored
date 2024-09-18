2dc4a68769aeb53ad004e3794909da934c347137
Sofia Ex <sophia.exarchou@icloud.com>
2024-09-19 00:03:00 +0200

    improve style

--- src/review-modal.js
+++ src/review-modal.js
@@ -4,18 +4,18 @@ export const createReviewFormularView = () => {
 	// title
 	const mainTitleView = document.createElement("h1");
 	mainTitleView.textContent = "Your opinion matters";
-	mainTitleView.classList.add("bg-[rgba(125,145,157,60)]", "m-4", "p-2", "text-white", "rounded-xl");
+	mainTitleView.classList.add("text-white", "rounded-xl", "text-center", "text-4xl");
 	reviewContainer.appendChild(mainTitleView);
 
-	const titleView = document.createElement("h1");
-	titleView.textContent = "Did you enjoy this film?";
-	titleView.classList.add("font-sans", "text-white", "text-center");
-	reviewContainer.appendChild(titleView);
+	// const titleView = document.createElement("h1");
+	// titleView.textContent = "Did you enjoy this film?";
+	// titleView.classList.add("font-sans", "text-white", "text-center");
+	// reviewContainer.appendChild(titleView);
 
-	const textView = document.createElement("p");
-	textView.textContent = "tell us what you thought";
-	textView.classList.add("font-sans", "text-center", "text-white");
-	reviewContainer.appendChild(textView);
+	// const textView = document.createElement("p");
+	// textView.textContent = "tell us what you thought";
+	// textView.classList.add("font-sans", "text-center", "text-white");
+	// reviewContainer.appendChild(textView);
 
 	// create emoji buttons
 	const emojiContainer = document.createElement("ul");
@@ -25,6 +25,7 @@ export const createReviewFormularView = () => {
 		"space-x-2",
 		"items-center",
 		"justify-center",
+		"p-4",
 		"w-full"
 	);
 
@@ -43,7 +44,9 @@ export const createReviewFormularView = () => {
 			"duration-300",
 			"hover:scale-150",
 			"active:border-none",
-			"opacity-50"
+			"opacity-50",
+			"w-full",
+			"py-4"
 		);
 
 		emojiButton.onclick = (event) => {
@@ -68,40 +71,28 @@ export const createReviewFormularView = () => {
 	const reviewTextArea = document.createElement("textarea");
 	reviewTextArea.id = "reviewTextArea";
 	reviewTextArea.classList.add(
-		"flex",
-		"flex-col",
-		"resize-none",
-		"items-center",
-		"border-2",
-		"border-green",
 		"rounded-2xl",
-		"p-6",
-		"w-1/2",
+		"p-4",
+		"w-full",
 		"mx-auto"
 	);
-	reviewTextArea.rows = 4;
+	reviewTextArea.rows = 6
 	reviewTextArea.placeholder = "Write your review here";
 	reviewContainer.appendChild(reviewTextArea);
-
-
 
 	const submitReviewButton = document.createElement("button");
 	submitReviewButton.type = "submit";
 	submitReviewButton.classList.add(
 		"font-2xl",
-		"rounded-3xl",
+		"rounded-2xl",
 		"bg-[#020F1D]",
 		"text-white",
 		"m-6",
 		"p-2",
-		"w-1/2",
-		"transform",
-		"transition-transform",
-		"ease-linear",
 		"duration-300",
-		"hover:scale-110",
 		"mx-auto",
-		"block"
+		"block",
+		"w-full",
 	);
 	//  < mt-[1rem] py-[5px] px-[2rem] text-[1.4rem] bg-[#020F1D] rounded-full">+ Add to favorites</button>
 	submitReviewButton.textContent = "Submit your review";
@@ -117,19 +108,13 @@ export const createReviewFormularView = () => {
 	const cancelButton = document.createElement("button");
 	cancelButton.classList.add(
 		"font-2xl",
-		"rounded-3xl",
+		"rounded-2xl",
 		"bg-[#020F1D]",
 		"text-white",
-		"m-6",
 		"p-2",
-		"w-1/2",
-		"transform",
-		"transition-transform",
-		"ease-linear",
-		"duration-300",
-		"hover:scale-110",
 		"mx-auto",
-		"block"
+		"block",
+		"w-full",
 	);
 	cancelButton.textContent = "Maybe later";
 	cancelButton.onclick = () => {
